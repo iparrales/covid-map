6eb36838698dd9f0d7ef30a3d6bd055a87cc4fc3
kyleslater <37429591+kyleslater@users.noreply.github.com>
2020-12-20 02:23:16 -0800

    Finished US Map

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_usaLow from "@amcharts/amcharts4-geodata/usaLow";
-import {fetchMapData} from '../api';
+import {
+  fetchMapData
+} from '../api';
 import axios from 'axios'
 
 am4core.useTheme(am4themes_animated);
 
 var stateData;
-
-var stateData2 = fetchMapData();
 
 axios.get('https://disease.sh/v3/covid-19/states')
   .then(function (response) {
@@ -26,286 +25,297 @@ axios.get('https://disease.sh/v3/covid-19/states')
     // always executed
   });
 
+
 class App extends React.Component {
-  componentDidMount() {
-    let chart = am4core.create("chartdiv", am4maps.MapChart);
+    componentDidMount() {
+      console.log(stateData);
+      let chart = am4core.create("chartdiv", am4maps.MapChart);
 
-    // Set map definition
-    chart.geodata = am4geodata_usaLow;
+      // Set map definition
+      chart.geodata = am4geodata_usaLow;
 
-    // Set projection
-    chart.projection = new am4maps.projections.AlbersUsa();
+      // Set projection
+      chart.projection = new am4maps.projections.AlbersUsa();
 
-    // Create map polygon series
-    let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+      // Create map polygon series
+      let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
-    //Set min/max fill color for each area
-    polygonSeries.heatRules.push({
-      property: "fill",
-      target: polygonSeries.mapPolygons.template,
-      min: chart.colors.getIndex(1).brighten(1),
-      max: chart.colors.getIndex(1).brighten(-0.3)
-    });
+      //Set min/max fill color for each area
+      polygonSeries.heatRules.push({
+        property: "fill",
+        target: polygonSeries.mapPolygons.template,
+        min: chart.colors.getIndex(1).brighten(1),
+        max: chart.colors.getIndex(1).brighten(-0.3)
+      });
 
-    // Make map load polygon data (state shapes and names) from GeoJSON
-    polygonSeries.useGeodata = true;
+      // Make map load polygon data (state shapes and names) from GeoJSON
+      polygonSeries.useGeodata = true;
 
-    // Set heatmap values for each state
-    polygonSeries.data = [{
-        id: "US-AL",
-        stateName: "Alabama"
-      },
-      {
-        id: "US-AK",
-        value: 626932
-      },
-      {
-        id: "US-AZ",
-        value: 5130632
-      },
-      {
-        id: "US-AR",
-        value: 2673400
-      },
-      {
-        id: "US-CA",
-        value: 33871648
-      },
-      {
-        id: "US-CO",
-        value: 4301261
-      },
-      {
-        id: "US-CT",
-        value: 3405565
-      },
-      {
-        id: "US-DE",
-        value: 783600
-      },
-      {
-        id: "US-FL",
-        value: 15982378
-      },
-      {
-        id: "US-GA",
-        value: 8186453
-      },
-      {
-        id: "US-HI",
-        value: 1211537
-      },
-      {
-        id: "US-ID",
-        value: 1293953
-      },
-      {
-        id: "US-IL",
-        value: 12419293
-      },
-      {
-        id: "US-IN",
-        value: 6080485
-      },
-      {
-        id: "US-IA",
-        value: 2926324
-      },
-      {
-        id: "US-KS",
-        value: 2688418
-      },
-      {
-        id: "US-KY",
-        value: 4041769
-      },
-      {
-        id: "US-LA",
-        value: 4468976
-      },
-      {
-        id: "US-ME",
-        value: 1274923
-      },
-      {
-        id: "US-MD",
-        value: 5296486
-      },
-      {
-        id: "US-MA",
-        value: 6349097
-      },
-      {
-        id: "US-MI",
-        value: 9938444
-      },
-      {
-        id: "US-MN",
-        value: 4919479
-      },
-      {
-        id: "US-MS",
-        value: 2844658
-      },
-      {
-        id: "US-MO",
-        value: 5595211
-      },
-      {
-        id: "US-MT",
-        value: 902195
-      },
-      {
-        id: "US-NE",
-        value: 1711263
-      },
-      {
-        id: "US-NV",
-        value: 1998257
-      },
-      {
-        id: "US-NH",
-        value: 1235786
-      },
-      {
-        id: "US-NJ",
-        value: 8414350
-      },
-      {
-        id: "US-NM",
-        value: 1819046
-      },
-      {
-        id: "US-NY",
-        value: 18976457
-      },
-      {
-        id: "US-NC",
-        value: 8049313
-      },
-      {
-        id: "US-ND",
-        value: 642200
-      },
-      {
-        id: "US-OH",
-        value: 11353140
-      },
-      {
-        id: "US-OK",
-        value: 3450654
-      },
-      {
-        id: "US-OR",
-        value: 3421399
-      },
-      {
-        id: "US-PA",
-        value: 12281054
-      },
-      {
-        id: "US-RI",
-        value: 1048319
-      },
-      {
-        id: "US-SC",
-        value: 4012012
-      },
-      {
-        id: "US-SD",
-        value: 754844
-      },
-      {
-        id: "US-TN",
-        value: 5689283
-      },
-      {
-        id: "US-TX",
-        value: 20851820
-      },
-      {
-        id: "US-UT",
-        value: 2233169
-      },
-      {
-        id: "US-VT",
-        value: 608827
-      },
-      {
-        id: "US-VA",
-        value: 7078515
-      },
-      {
-        id: "US-WA",
-        value: 5894121
-      },
-      {
-        id: "US-WV",
-        value: 1808344
-      },
-      {
-        id: "US-WI",
-        value: 5363675
-      },
-      {
-        id: "US-WY",
-        value: 493782
+      // Correspond state IDs to statename
+      polygonSeries.data = [{
+          id: "US-AL",
+          stateName: "Alabama"
+        },
+        {
+          id: "US-AK",
+          stateName: "Alaska"
+        },
+        {
+          id: "US-AZ",
+          stateName: "Arizona"
+        },
+        {
+          id: "US-AR",
+          stateName: "Arkansas"
+        },
+        {
+          id: "US-CA",
+          stateName: "California"
+        },
+        {
+          id: "US-CO",
+          stateName: "Colorado"
+        },
+        {
+          id: "US-CT",
+          stateName: "Connecticut"
+        },
+        {
+          id: "US-DE",
+          stateName: "Delaware"
+        },
+        {
+          id: "US-FL",
+          stateName: "Florida"
+        },
+        {
+          id: "US-GA",
+          stateName: "Georgia"
+        },
+        {
+          id: "US-HI",
+          stateName: "Hawaii"
+        },
+        {
+          id: "US-ID",
+          stateName: "Idaho"
+        },
+        {
+          id: "US-IL",
+          stateName: "Illinois"
+        },
+        {
+          id: "US-IN",
+          stateName: "Indiana"
+        },
+        {
+          id: "US-IA",
+          stateName: "Iowa"
+        },
+        {
+          id: "US-KS",
+          stateName: "Kansas"
+        },
+        {
+          id: "US-KY",
+          stateName: "Kentucky"
+        },
+        {
+          id: "US-LA",
+          stateName: "Louisiana"
+        },
+        {
+          id: "US-ME",
+          stateName: "Maine"
+        },
+        {
+          id: "US-MD",
+          stateName: "Maryland"
+        },
+        {
+          id: "US-MA",
+          stateName: "Massachusetts"
+        },
+        {
+          id: "US-MI",
+          stateName: "Michigan"
+        },
+        {
+          id: "US-MN",
+          stateName: "Minnesota"
+        },
+        {
+          id: "US-MS",
+          stateName: "Mississippi"
+        },
+        {
+          id: "US-MO",
+          stateName: "Missouri"
+        },
+        {
+          id: "US-MT",
+          stateName: "Montana"
+        },
+        {
+          id: "US-NE",
+          stateName: "Nebraska"
+        },
+        {
+          id: "US-NV",
+          stateName: "Nevada"
+        },
+        {
+          id: "US-NH",
+          stateName: "New Hampshire"
+        },
+        {
+          id: "US-NJ",
+          stateName: "New Jersey"
+        },
+        {
+          id: "US-NM",
+          stateName: "New Mexico"
+        },
+        {
+          id: "US-NY",
+          stateName: "New York"
+        },
+        {
+          id: "US-NC",
+          stateName: "North Carolina"
+        },
+        {
+          id: "US-ND",
+          stateName: "North Dakota"
+        },
+        {
+          id: "US-OH",
+          stateName: "Ohio"
+        },
+        {
+          id: "US-OK",
+          stateName: "Oklahoma"
+        },
+        {
+          id: "US-OR",
+          stateName: "Oregon"
+        },
+        {
+          id: "US-PA",
+          stateName: "Pennsylvania"
+        },
+        {
+          id: "US-RI",
+          stateName: "Rhode Island"
+        },
+        {
+          id: "US-SC",
+          stateName: "South Carolina"
+        },
+        {
+          id: "US-SD",
+          stateName: "South Dakota"
+        },
+        {
+          id: "US-TN",
+          stateName: "Tennessee"
+        },
+        {
+          id: "US-TX",
+          stateName: "Texas"
+        },
+        {
+          id: "US-UT",
+          stateName: "Utah"
+        },
+        {
+          id: "US-VT",
+          stateName: "Vermont"
+        },
+        {
+          id: "US-VA",
+          stateName: "Virginia"
+        },
+        {
+          id: "US-WA",
+          stateName: "Washington"
+        },
+        {
+          id: "US-WV",
+          stateName: "West Virginia"
+        },
+        {
+          id: "US-WI",
+          stateName: "Wisconsin"
+        },
+        {
+          id: "US-WY",
+          stateName: "Wyoming"
+        }
+      ];
+
+
+      //Attach Covid Data to States
+      var stName;
+      var x;
+      console.log(polygonSeries.data);
+      for (let i = 0; i < polygonSeries.data.length; i++) {
+        //console.log(polygonSeries.data[i].stateName);
+        stName = polygonSeries.data[i].stateName;
+        x = stateData.filter((y) => y.state === stName)
+        //console.log(x[0].cases);
+        polygonSeries.data[i].value = x[0].cases;
+        polygonSeries.data[i].cases = x[0].cases;
+        polygonSeries.data[i].deaths = x[0].deaths;
+        polygonSeries.data[i].recovered = x[0].recovered;
       }
-    ];
+      console.log(polygonSeries.data[0].stateName);
+      console.log(stateData);
 
-    polygonSeries.data.forEach(i => {
-      //let stName = polygonSeries.data[i].stateName;
 
-    });
-    console.log(polygonSeries.data[0].stateName);
-    console.log(stateData);
-    let x = stateData.filter((y) => y.state === "California")
-    console.log(x.cases);
+      // Set up heat legend
+      let heatLegend = chart.createChild(am4maps.HeatLegend);
+      heatLegend.series = polygonSeries;
+      heatLegend.align = "right";
+      heatLegend.valign = "bottom";
+      heatLegend.width = am4core.percent(20);
+      heatLegend.marginRight = am4core.percent(4);
+      heatLegend.minValue = 0;
+      heatLegend.maxValue = 2000000;
 
-    // Set up heat legend
-    let heatLegend = chart.createChild(am4maps.HeatLegend);
-    heatLegend.series = polygonSeries;
-    heatLegend.align = "right";
-    heatLegend.valign = "bottom";
-    heatLegend.width = am4core.percent(20);
-    heatLegend.marginRight = am4core.percent(4);
-    heatLegend.minValue = 0;
-    heatLegend.maxValue = 40000000;
+      // Set up custom heat map legend labels using axis ranges
+      let minRange = heatLegend.valueAxis.axisRanges.create();
+      minRange.value = heatLegend.minValue;
+      minRange.label.text = "0 Cases";
+      let maxRange = heatLegend.valueAxis.axisRanges.create();
+      maxRange.value = heatLegend.maxValue;
+      maxRange.label.text = "2,000,000 Cases";
 
-    // Set up custom heat map legend labels using axis ranges
-    let minRange = heatLegend.valueAxis.axisRanges.create();
-    minRange.value = heatLegend.minValue;
-    minRange.label.text = "Little";
-    let maxRange = heatLegend.valueAxis.axisRanges.create();
-    maxRange.value = heatLegend.maxValue;
-    maxRange.label.text = "A lot!";
+      // Blank out internal heat legend value axis labels
+      heatLegend.valueAxis.renderer.labels.template.adapter.add("text", function (labelText) {
+        return "";
+      });
 
-    // Blank out internal heat legend value axis labels
-    heatLegend.valueAxis.renderer.labels.template.adapter.add("text", function (labelText) {
-      return "";
-    });
+      // Configure series tooltip
+      let polygonTemplate = polygonSeries.mapPolygons.template;
+      polygonTemplate.tooltipText = "{name}\nCases: {cases}\nDeaths: {deaths}\nRecovered: {recovered}";
+      polygonTemplate.nonScalingStroke = true;
+      polygonTemplate.strokeWidth = 0.5;
 
-    // Configure series tooltip
-    let polygonTemplate = polygonSeries.mapPolygons.template;
-    polygonTemplate.tooltipText = "{name}\nCases: 1,000,000\nDeaths: 10,000\nRecovered: 500,000";
-    polygonTemplate.nonScalingStroke = true;
-    polygonTemplate.strokeWidth = 0.5;
+      // Create hover state and set alternative fill color
+      let hs = polygonTemplate.states.create("hover");
+      hs.properties.fill = am4core.color("#3c5bdc");
+    }
 
-    // Create hover state and set alternative fill color
-    let hs = polygonTemplate.states.create("hover");
-    hs.properties.fill = am4core.color("#3c5bdc");
-  }
+    componentWillUnmount() {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    }
 
-  componentWillUnmount() {
-    if (this.chart) {
-      this.chart.dispose();
+    render() {
+      return (<div id = "chartdiv" style = {{width: "100%", height: "90vh"}}></div>);
     }
   }
 
-  render() {
-    return ( <div id = "chartdiv" style = {{width: "100%", height: "500px"}}></div>
-    );
-  }
-}
-
-export default App;
+  export default App;
